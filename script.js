789bf6b8080d7eb472bd8ab1673b2d63ddd47e54
Bruno Marques <besmarques@gmail.com>
2021-11-30 12:31:49 +0000

    finished

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function displayGrid(){
     var classLength = document.getElementsByClassName("card").length;
     for (var i = 0; i < classLength; i++){
-        document.getElementsByClassName("card")[i].className = "card col-4"
+        document.getElementsByClassName("main")[0].className = "main container-fluid"
+        document.getElementsByClassName("card")[i].className = "card col-4 g-0 mb-0"
+        document.getElementsByClassName("card-title")[i].className = "card-title row g-2 justify-content-around d-none"
+        document.getElementsByClassName("card-bottom")[i].className = "card-bottom d-none"
     }
 }
 
 function displayList(){
     var classLength = document.getElementsByClassName("card").length;
     for (var i = 0; i < classLength; i++){
-        document.getElementsByClassName("card")[i].className = "card col-7" 
+        document.getElementsByClassName("main")[0].className = "main container"
+        document.getElementsByClassName("card")[i].className = "card col-7 mt-5 mb-3 border-0"
+        document.getElementsByClassName("card-title")[i].className = "card-title row g-2 justify-content-around pt-2"
+        document.getElementsByClassName("card-bottom")[i].className = "card-bottom"
     }
 }
